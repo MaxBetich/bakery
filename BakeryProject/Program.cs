38135f7e736f3a257e396164baa29b161aab1f7a
Max Betich <betichm@gmail.com>
2023-05-05 14:00:03 -0700

    update program with ui elements

--- BakeryProject/Program.cs
+++ BakeryProject/Program.cs
@@ -7,7 +7,16 @@ namespace Bakery
   {
     static void Main()
     {
-      
+      Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~");
+      Console.WriteLine("Welcome to your friendly neighborhood bakery!");
+      Console.WriteLine("Our specialties for today are freshly-baked loaves of bread, and a wide variety of pastries");
+      Console.WriteLine("Prices:");
+      Console.WriteLine("Bread: $5/loaf");
+      Console.WriteLine("Pastries: $2/item");
+      Console.WriteLine("We currently have two specials running:");
+      Console.WriteLine("For every two loaves of bread purchased, we will include a third loaf for free!");
+      Console.WriteLine("For every three pastries purchased, we will include a fourth pastry of your choice for free!");
+      Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~");
     }
   }
 }
