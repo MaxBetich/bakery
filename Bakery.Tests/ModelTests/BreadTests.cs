4e1e8418e840be97bc5a674eb9f8b45d33b4dd65
Max Betich <betichm@gmail.com>
2023-05-05 08:34:37 -0700

    add test for bread count property

--- Bakery.Tests/ModelTests/BreadTests.cs
+++ Bakery.Tests/ModelTests/BreadTests.cs
@@ -12,5 +12,14 @@ namespace Bakery.Tests
       Bread newBread = new Bread();
       Assert.AreEqual(typeof(Bread), newBread.GetType());
     }
+
+    [TestMethod]
+    public void SetCount_SetsValueOfCount_Void()
+    {
+      Bread newBread = new Bread();
+      int newCount = 5;
+      newBread.Count = newCount;
+      Assert.AreEqual(newCount, newBread.Count);
+    }
   }
 }
